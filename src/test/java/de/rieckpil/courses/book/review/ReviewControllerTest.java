c140a02f760e7390782cc09ddcd5148f24f55b8d
Adorjánné Balogh Judit <balogh.judit@webstar.hu>
2025-04-28 22:48:40 +0200

    adding advanced web layer tests

--- src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
@@ -1,16 +1,27 @@
 package de.rieckpil.courses.book.review;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import de.rieckpil.courses.config.WebSecurityConfig;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,28 +35,123 @@ class ReviewControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
+
   private ObjectMapper objectMapper;
 
+  @BeforeEach
+  public void beforeEach() {
+    this.objectMapper = new ObjectMapper();
+  }
   @Test
-  void shouldReturnTwentyReviewsWithoutAnyOrderWhenNoParametersAreSpecified() throws Exception {}
+  void shouldReturnTwentyReviewsWithoutAnyOrderWhenNoParametersAreSpecified() throws Exception {
+
+    ArrayNode result = objectMapper.createArrayNode();
+    ObjectNode statistic = objectMapper.createObjectNode();
+    statistic.put("bookId", 1);
+    statistic.put("isbn", "42");
+    statistic.put("avg", 89.3);
+    statistic.put("ratings", 2);
+
+    result.add(statistic);
+
+    when(reviewService.getAllReviews(20, "none")).thenReturn(result);
+    this.mockMvc
+      .perform(get("/api/books/reviews"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.size()", Matchers.is(1)));
+  }
 
   @Test
-  void shouldNotReturnReviewStatisticsWhenUserIsUnauthenticated() throws Exception {}
+  void shouldNotReturnReviewStatisticsWhenUserIsUnauthenticated() throws Exception {
+    this.mockMvc
+      .perform(get("/api/books/reviews/statistics"))
+      .andExpect(status().isUnauthorized());
+  }
 
   @Test
-  void shouldReturnReviewStatisticsWhenUserIsAuthenticated() throws Exception {}
+  //@WithMockUser(username = "duke", roles = "user")
+  void shouldReturnReviewStatisticsWhenUserIsAuthenticated() throws Exception {
+
+    this.mockMvc
+      .perform(get("/api/books/reviews/statistics")
+        //.with(user("duke")))
+        //.with(httpBasic("duke", "password"))
+        .with(jwt()))
+      .andExpect(status().isOk());
+
+    verify(reviewService).getReviewStatistics();
+  }
 
   @Test
-  void shouldCreateNewBookReviewForAuthenticatedUserWithValidPayload() throws Exception {}
+  void shouldCreateNewBookReviewForAuthenticatedUserWithValidPayload() throws Exception {
+
+    String requestBody = """
+        {
+          "reviewTitle": "Great Java Book!",
+          "reviewContent": "I really like this book!",
+          "rating": 5
+         }
+      """;
+
+    when(reviewService.createBookReview(
+      eq("42"),
+      any(BookReviewRequest.class),
+      eq("duke"),
+      endsWith("spring.io"))
+    ).thenReturn(84L);
+
+    this.mockMvc
+          .perform(post("/api/books/{isbn}/reviews", 42)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestBody)
+            .with(jwt().jwt((builder) -> builder
+              .claim("email", "duke@spring.io")
+              .claim("preferred_username", "duke"))))
+      .andExpect(status().isCreated())
+      .andExpect(header().exists("Location"))
+      .andExpect(header().string("Location", Matchers.containsString("books/42/reviews/84")));
+  }
 
   @Test
-  void shouldRejectNewBookReviewForAuthenticatedUsersWithInvalidPayload() throws Exception {}
+  void shouldRejectNewBookReviewForAuthenticatedUsersWithInvalidPayload() throws Exception {
+
+    String requestBody = """
+        {
+          "reviewContent": "I really like this book!",
+          "rating": -5
+         }
+      """;
+
+    this.mockMvc
+      .perform(post("/api/books/{isbn}/reviews", 42)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestBody)
+        .with(jwt().jwt((builder) -> builder
+          .claim("email", "duke@spring.io")
+          .claim("preferred_username", "duke"))))
+      .andExpect(status().isBadRequest())
+      .andDo(MockMvcResultHandlers.print());
+  }
 
   @Test
-  void shouldNotAllowDeletingReviewsWhenUserIsAuthenticatedWithoutModeratorRole()
-      throws Exception {}
+  void shouldNotAllowDeletingReviewsWhenUserIsAuthenticatedWithoutModeratorRole() throws Exception {
+    this.mockMvc
+      .perform(delete("/api/books/{isbn}/reviews/{reviewId}", 42, 3)
+        .with(jwt()))
+        .andExpect(status().isForbidden());
+
+    verifyNoInteractions(reviewService);
+  }
 
   @Test
   @WithMockUser(roles = "moderator")
-  void shouldAllowDeletingReviewsWhenUserIsAuthenticatedAndHasModeratorRole() throws Exception {}
+  void shouldAllowDeletingReviewsWhenUserIsAuthenticatedAndHasModeratorRole() throws Exception {
+    this.mockMvc
+      .perform(delete("/api/books/{isbn}/reviews/{reviewId}", 42, 3)
+        //.with(jwt().authorities(new SimpleGrantedAuthority("ROLE_moderator")))
+      )
+        .andExpect(status().isOk());
+
+    verify(reviewService).deleteReview("42", 3L);
+  }
 }
